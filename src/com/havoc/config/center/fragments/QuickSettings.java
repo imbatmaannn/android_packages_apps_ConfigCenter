8a8039d6a0aa44449f68f04a6aee427805f3bd34
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-06 00:36:46 +0530

    QS themes [2/2]
    QS: Allow coloring bg with accent [2/2]
    
    Xtensions: Set QS BG alpha even with set qs from resources [2/2]
    
    [ @HrX03 - POSP ]
    
    - Yet again, the title explains it

--- src/com/havoc/config/center/fragments/QuickSettings.java
+++ src/com/havoc/config/center/fragments/QuickSettings.java
@@ -30,6 +30,7 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.havoc.support.preferences.CustomSeekBarPreference;
 import com.havoc.support.preferences.SystemSettingMasterSwitchPreference;
+import com.havoc.support.colorpicker.ColorPickerPreference;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -43,6 +44,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
     private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
     private static final String QS_BACKGROUND_BLUR = "qs_background_blur";
+    private static final String QS_PANEL_COLOR = "qs_panel_color";
+    static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
 
     private CustomSeekBarPreference mQsColumnsPortrait;
     private CustomSeekBarPreference mQsColumnsLandscape;
@@ -51,6 +54,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private CustomSeekBarPreference mQsRowsLandscape;
     private SystemSettingMasterSwitchPreference mCustomHeader;
     private SystemSettingMasterSwitchPreference mQsBlur;
+    private ColorPickerPreference mQsPanelColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -88,6 +92,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_QUICKBAR_COLUMNS, 6);
         mQsColumnsQuickbar.setValue(columnsQuickbar);
         mQsColumnsQuickbar.setOnPreferenceChangeListener(this);	
+
+        mQsPanelColor = (ColorPickerPreference) findPreference(QS_PANEL_COLOR);
+        mQsPanelColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.QS_PANEL_BG_COLOR, DEFAULT_QS_PANEL_COLOR, UserHandle.USER_CURRENT);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQsPanelColor.setSummary(hexColor);
+        mQsPanelColor.setNewPreviewColor(intColor);
 
         updateMasterPrefs();
     }
@@ -143,6 +155,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             boolean blur = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(),
                     Settings.System.QS_BACKGROUND_BLUR, blur ? 1 : 0);
+            return true;
+        } else if (preference == mQsPanelColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.QS_PANEL_BG_COLOR, intHex, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
